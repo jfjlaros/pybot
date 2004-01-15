00d114607b56c22f098f20558b10d1fb146296bb
wichert <wichert@wiggy.net>
2004-01-15 22:50:15 +0000

    More updates
    git-svn-id: file:///home/bot/dump/pybot@14 584a7a8f-98c9-0310-aba5-a1f99c0cc8ce

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 # __init__.py
 #
-# Copyright 2003 Wichert Akkerman <wichert@wiggy.net>
+# Copyright 2003,2004 Wichert Akkerman <wichert@wiggy.net>
 
 __docformat__   = "epytext en"
 __author__      = "Wichert Akkerman <wichert@wiggy.net>"
-__copyright__   = "Copyright 2003 Wichert Akkerman"
+__copyright__   = "Copyright 2003,2004 Wichert Akkerman"
 
-__all__		= [ "basicbot", "ircbot", "irclib", "tbf" ]
+__all__		= [ "ircbot", "irclib", "nicktrack", "sqlbot", "tbf" ]
