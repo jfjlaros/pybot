fbe9f0607d223d68880ca3c2081cefffdc027798
wichert <wichert@wiggy.net>
2004-01-18 17:28:02 +0000

    Use more accurate method to detect URLs
    git-svn-id: file:///home/bot/dump/pybot@22 584a7a8f-98c9-0310-aba5-a1f99c0cc8ce

--- src/logbot.py
+++ src/logbot.py
@@ -1,9 +1,15 @@
 # urlbot.py
 
-from dhm import sqlwrap
+import re
 import irclib, sqlbot
 
 class LogBot(sqlbot.SQLBot):
+	urlmatcher      = re.compile(
+		r"\b(?P<url>(?P<scheme>http|https|ftp)://"
+		r"(?:(?P<login>(?P<username>[a-zA-Z0-9]+)(?::(?P<password>[A-Za-z0-9]+))?)@)?"
+		r"(?P<hostname>[A-Za-z0-9.-]+(?::(?P<port>[0-9]+))?)"
+		r"(?P<path>[A-Za-z0-9@~=?/.&;#+-]*))")
+
 	def __init__(self, *args, **kwargs):
 		super(LogBot, self).__init__(*args, **kwargs)
 
@@ -12,15 +18,19 @@ class LogBot(sqlbot.SQLBot):
 				self.config["SQL/table"])
 
 
+	def hasurl(self, text):
+		return self.urlmatcher.match(text)!=None
+
+
 	def _urlbot_pubmsg(self, connection, event):
 		nick=irclib.nm_to_n(event.source())
 		msg=event.arguments()[0]
 
 		try:
 			self.sqlverify()
-			self.dbc.execute(self.logcmd, 
-				(("http" in msg or "https" in msg or "ftp:" in msg),
-				 nick, msg), "format")
+			url=self.hasurl(msg)
+
+			self.dbc.execute(self.logcmd, (url,nick,msg), "format")
 			self.dbc.commit()
 		except self.dbc.Error, e:
 			self.logger.error("Failed to log message: %s" % e)
